a13f3afae3c6b107d3899cb55e868e1025d595c9
lz-coder <lzcoder@proton.me>
2023-02-18 00:47:08 -0300

    Update .config/labwc/autostart Update .config/labwc/menu.xml Update .config/labwc/rc.xml Add .config/labwc/scripts/gsettings-theme-setter.sh

---
+++ dot_config/labwc/scripts/executable_gsettings-theme-setter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
+gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
+gsettings set org.gnome.desktop.interface cursor-theme "retrosmart"
+gsettings set org.gnome.desktop.interface font-name "Roboto 10"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
